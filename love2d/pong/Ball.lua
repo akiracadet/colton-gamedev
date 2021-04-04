aa0d055b6f6f100b340637c1da1c5f61a3998d34
Von Delos Santos <vdelossantos@you-source.com>
2021-04-04 20:21:45 +0800

    Added collider.

--- love2d/pong/Ball.lua
+++ love2d/pong/Ball.lua
@@ -12,9 +12,24 @@ function Ball:init(x, y, width, height)
     self.dx = math.random(-50, 50)
 end
 
+function Ball:collides(paddle)
+    -- first, check to see if the left edge of either is farther to the right
+    -- than the right edge of the other
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either is higher than the top
+    -- edge of the other
+    if self.y > paddle.y + paddle.height or paddle.y > self.x + self.height then
+        return false
+    end
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
 function Ball:reset()
-    -- places the ball in the middle of the screen, with an initial random velocity
-    -- on both axes.
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
     self.dy = math.random(2) == 1 and -100 or 100
@@ -22,7 +37,6 @@ function Ball:reset()
 end
 
 function Ball:update(dt)
-    -- simply applies velocity to position, scaled by deltaTime
     self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
 end
